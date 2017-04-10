e2cc0bed970f151525c57fd2af4c0aacd1ceed50
zk_chs <zkchs1991@gmail.com>
2017-04-10 20:57:28 +0800

    init

---
+++ researches-spring-boot/spring-boot-jsp/src/main/java/com/zk/sbt/jsp/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.zk.sbt.jsp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+/**
+ * Created by zk_chs on 4/10/17.
+ */
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("name", "world!");
+        return "welcome";
+    }
+
+}
