f5a6580b743ed992cf53226b2ef82d62c2c44704
zk_chs <zkchs1991@gmail.com>
2017-04-22 23:22:03 +0800

    spring-boot-mvc add spring-boot-service dependency

---
+++ researches-spring-boot/spring-boot-mvc/src/main/java/com/zk/sbt/controller/DeveloperController.java
@@ -0,0 +1,30 @@
+package com.zk.sbt.controller;
+
+import com.zk.sbt.entity.Developer;
+import com.zk.sbt.service.interfaces.DeveloperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.*;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+/**
+ * Created by zk_chs on 4/22/17.
+ */
+@RestController
+@RequestMapping(value = "/developers", produces = APPLICATION_JSON_UTF8_VALUE)
+public class DeveloperController {
+
+    @Autowired
+    private DeveloperService developerService;
+
+    @RequestMapping(value = "/{developerId}", method = GET)
+    public ResponseEntity<Developer> findById (@PathVariable("developerId") Long developerId){
+        Developer developer = developerService.findById(developerId);
+        return ResponseEntity.ok(developer);
+    }
+
+}
