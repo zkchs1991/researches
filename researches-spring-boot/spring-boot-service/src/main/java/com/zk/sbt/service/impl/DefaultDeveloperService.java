2804a558f3c3e34533c0e83f1fe946ef947361dd
zk_chs <zkchs1991@gmail.com>
2017-04-22 23:16:14 +0800

    spring-boot-service add dao dependency

---
+++ researches-spring-boot/spring-boot-service/src/main/java/com/zk/sbt/service/impl/DefaultDeveloperService.java
@@ -0,0 +1,26 @@
+package com.zk.sbt.service.impl;
+
+import com.zk.sbt.entity.Developer;
+import com.zk.sbt.mybatis.dao.DeveloperDao;
+import com.zk.sbt.service.interfaces.DeveloperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by zk_chs on 4/22/17.
+ */
+@Service
+@Transactional
+public class DefaultDeveloperService implements DeveloperService {
+
+    @Autowired
+    private DeveloperDao developerDao;
+
+    @Override
+    public Developer findById(Long developerId) {
+        Developer developer = developerDao.findById(developerId);
+        return developer;
+    }
+
+}
---
+++ researches-spring-boot/spring-boot-service/src/main/java/com/zk/sbt/service/interfaces/DeveloperService.java
@@ -0,0 +1,12 @@
+package com.zk.sbt.service.interfaces;
+
+import com.zk.sbt.entity.Developer;
+
+/**
+ * Created by zk_chs on 4/22/17.
+ */
+public interface DeveloperService {
+
+    Developer findById(Long developerId);
+
+}
