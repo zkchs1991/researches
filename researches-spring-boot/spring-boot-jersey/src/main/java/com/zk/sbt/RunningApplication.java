04411fc4900ca268d860f1953328a68c602324fc
zk_chs <zkchs1991@gmail.com>
2017-04-22 21:22:57 +0800

    spring-boot-jersey add spring-boot-jpa dependency

--- researches-spring-boot/spring-boot-jersey/src/main/java/com/zk/sbt/RunningApplication.java
+++ researches-spring-boot/spring-boot-jersey/src/main/java/com/zk/sbt/RunningApplication.java
@@ -1,7 +1,7 @@
 package com.zk.sbt;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Created by zk_chs on 4/20/17.
@@ -9,9 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RunningApplication {
 
+    /**
+     * 关于多个properties,下面的是一种配置方式
+     * 也可以在需要的地方使用{@link org.springframework.context.annotation.PropertySource @PropertySource}
+     * 比如{@link com.zk.sbt.provider.DataSourceConfig DataSourceConfig}
+     */
     public static void main(String[] args) {
         System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
-        SpringApplication.run(RunningApplication.class, args);
+        new SpringApplicationBuilder(RunningApplication.class)
+                .properties("spring.config.name=application,datasource")
+                .run(args);
     }
 
 }
--- researches-spring-boot/spring-boot-jersey/src/main/java/com/zk/sbt/resource/apiv1/developer/DeveloperResource.java
+++ researches-spring-boot/spring-boot-jersey/src/main/java/com/zk/sbt/resource/apiv1/developer/DeveloperResource.java
@@ -1,10 +1,13 @@
 package com.zk.sbt.resource.apiv1.developer;
 
+import com.zk.sbt.entity.Developer;
+import com.zk.sbt.provider.data.developer.DeveloperProvider;
 import com.zk.sbt.resource.apiv1.developer.model.DeveloperRep;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
@@ -22,6 +25,9 @@ import javax.ws.rs.core.Response;
 @Api("开发者资源接口集合")
 public class DeveloperResource {
 
+    @Autowired
+    private DeveloperProvider developerProvider;
+
     @ApiOperation(value = "获取开发者信息接口")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "返回model", response = DeveloperRep.class, responseContainer = "List")
@@ -29,7 +35,8 @@ public class DeveloperResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response checkEnableEdit (@QueryParam("imageId") Long imageId){
-        return Response.ok().build();
+        Developer developer = developerProvider.findById(1L);
+        return Response.ok(developer).build();
     }
 
 }
