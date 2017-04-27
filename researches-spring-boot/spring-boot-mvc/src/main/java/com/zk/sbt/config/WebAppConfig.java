c15dadd48abeea69020477990a84b6475f946370
zkchs1991 <zkchs1991@gmail.com>
2017-04-27 18:26:32 +0800

    add druid statViewServlet to spring-boot-mvc

--- researches-spring-boot/spring-boot-mvc/src/main/java/com/zk/sbt/config/WebAppConfig.java
+++ researches-spring-boot/spring-boot-mvc/src/main/java/com/zk/sbt/config/WebAppConfig.java
@@ -1,8 +1,11 @@
 package com.zk.sbt.config;
 
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -59,6 +62,28 @@ public class WebAppConfig extends WebMvcConfigurerAdapter{
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return objectMapper;
+    }
+
+    /** druid自带监控 */
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+//        reg.addInitParameter("allow", "127.0.0.1"); //白名单
+//        reg.addInitParameter("deny",""); //黑名单
+        reg.addInitParameter("loginUsername", "zk_chs");
+        reg.addInitParameter("loginPassword", "zk_chs");
+        return reg;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
     }
 
 }
