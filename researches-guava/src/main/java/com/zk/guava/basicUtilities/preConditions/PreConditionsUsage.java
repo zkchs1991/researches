f8364476b5bf1fb03e94bfa67e861b1e23df97af
zk_chs <zkchs1991@gmail.com>
2017-04-27 23:08:22 +0800

    guava preCondition usage

---
+++ researches-guava/src/main/java/com/zk/guava/basicUtilities/preConditions/PreConditionsUsage.java
@@ -0,0 +1,28 @@
+package com.zk.guava.basicUtilities.preConditions;
+
+import static com.google.common.base.Preconditions.*;
+
+/**
+ * Created by zk_chs on 4/27/17.
+ * 感觉会用到的就是下面这几个了
+ */
+public class PreConditionsUsage {
+
+    public static void main(String[] args) {
+        checkArgument(getTrue());
+        checkArgument(getFalse(), "return value is false");
+
+        checkNotNull(getTrue());
+
+        checkState(getTrue());
+    }
+
+    private static boolean getTrue (){
+        return true;
+    }
+
+    private static boolean getFalse (){
+        return false;
+    }
+
+}
