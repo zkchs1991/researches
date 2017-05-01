97df0202012a5f58644f0adcf85d1584f20865af
zk_chs <zkchs1991@gmail.com>
2017-05-01 21:37:25 +0800

    static constructors of Collections Type

---
+++ researches-google/src/main/java/com/zk/google/guava/collections/collectionUtilities/StaticConstructorsUsage.java
@@ -0,0 +1,23 @@
+package com.zk.google.guava.collections.collectionUtilities;
+
+import com.google.common.collect.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by zk_chs on 5/1/17.
+ */
+public class StaticConstructorsUsage {
+
+    public static void main(String[] args) {
+        List<String> stringList = Lists.newArrayList("String1", "String2", "String3");
+        Set<String> stringSet = Sets.newHashSet();
+        Map<String, String> stringMap = Maps.newLinkedHashMap();
+
+        Multiset<String> stringMultiset = HashMultiset.create();
+        Multimap<String, String> stringStringMultimap = HashMultimap.create();
+    }
+
+}
