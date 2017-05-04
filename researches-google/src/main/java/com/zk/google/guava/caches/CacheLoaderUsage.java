730c8fa49658ad61c894be2f9194091f16da8207
zk_chs <zkchs1991@gmail.com>
2017-05-04 23:33:40 +0800

    guava cache's CacheLoader and cache's Callable  usage

--- researches-google/src/main/java/com/zk/google/guava/caches/CacheLoaderUsage.java
+++ researches-google/src/main/java/com/zk/google/guava/caches/CacheLoaderUsage.java
@@ -3,6 +3,7 @@ package com.zk.google.guava.caches;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.zk.google.gson.Result;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,69 +18,75 @@ import java.util.concurrent.TimeUnit;
 public class CacheLoaderUsage {
 
     private static final Logger log = LogManager.getLogger();
-    private static LoadingCache<String, String> caches;
+    private static LoadingCache<String, Result<String>> caches;
+    private static final String key = "key";
+    private static final Result<String> result = Result.DEFAULT_RESULT;
 
-    public static void main(String[] args) throws ExecutionException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         getCacheWithCheckedException();
+        log.info("after 5s to invoke another method");
+        Thread.sleep(5000);
         getCacheWithRuntimeException();
     }
 
     private static void getCacheWithCheckedException (){
         createCacheWithCheckedException();
-        String key = "key";
+
+        Result<String> value = null;
         try {
-            log.info("get value from cache, key is {}, value -> {}", key, caches.get(key));
+            value = caches.get(key);
         } catch (ExecutionException e) {
             log.error("meet the 1/2 rate to throw an exception");
         }
+        log.info("get value from cache, key is {}, value -> {}", key, value);
     }
 
     private static void createCacheWithCheckedException (){
         caches = CacheBuilder.newBuilder()
                 .maximumSize(1000)
                 .expireAfterWrite(10, TimeUnit.MINUTES)
-                .build(new CacheLoader<String, String>() {
+                .build(new CacheLoader<String, Result<String>>() {
                     @Override
-                    public String load(String key) throws Exception {
+                    public Result<String> load(String key) throws Exception {
                         return createExpensiveValueWithCheckedException(key);
                     }
                 });
     }
 
-    private static String createExpensiveValueWithCheckedException (String key) throws Exception {
+    private static Result<String> createExpensiveValueWithCheckedException (String key) throws Exception {
         long now = System.currentTimeMillis();
         if (now % 2 == 0){
             throw new Exception("50% rate to throw an exception");
         }
-        return "this is a big value of key " + key;
+        return result;
     }
 
     // If you have defined a CacheLoader that does not declare any checked exceptions
     // then you can perform cache lookups using getUnchecked(K);
     private static void getCacheWithRuntimeException (){
         createCacheWithRuntimeException();
-        String key = "key";
-        log.info("get value from cache, key is {}, value -> {}", key, caches.getUnchecked(key));
+        Result<String> value = caches.getUnchecked(key);
+        log.info("get value from cache, key is {}, value -> {}", key, value);
     }
 
     private static void createCacheWithRuntimeException (){
         caches = CacheBuilder.newBuilder()
                 .maximumSize(1000)
                 .expireAfterWrite(10, TimeUnit.MINUTES)
-                .build(new CacheLoader<String, String>() {
+                .build(new CacheLoader<String, Result<String>>() {
                     @Override
-                    public String load(String key) {
+                    public Result<String> load(String key) {
                         return createExceptionValueWithRuntimeException(key);
                     }
                 });
     }
 
-    private static String createExceptionValueWithRuntimeException (String key){
+    private static Result<String> createExceptionValueWithRuntimeException (String key){
         long now = System.currentTimeMillis();
         if (now % 2 == 0){
             throw new RuntimeException("50% rate to throw an exception");
         }
-        return "this is a big value of key " + key;
+        return result;
     }
 
 }
---
+++ researches-google/src/main/java/com/zk/google/guava/caches/CallableCacheUsage.java
@@ -0,0 +1,73 @@
+package com.zk.google.guava.caches;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.zk.google.gson.Result;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by zk_chs on 5/4/17.
+ *
+ * 详细文档地址 -> https://github.com/google/guava/wiki/CachesExplained
+ */
+public class CallableCacheUsage {
+
+    private static final Logger log = LogManager.getLogger();
+    private static Cache<String, Result<String>> cache;
+    private static final String key = "key";
+    private static final Result<String> result = Result.DEFAULT_RESULT;
+
+    public static void main(String[] args) throws InterruptedException {
+        useAsMapMethod();
+        log.info("after 5s to invoke another method");
+        Thread.sleep(5000);
+        useCallable();
+    }
+
+    private static void createCache (){
+        cache = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .build(); // look Ma, no CacheLoader
+    }
+
+    private static void useAsMapMethod (){
+        createCache();
+        Map<String, Result<String>> map = cache.asMap();
+        map.putIfAbsent(key, result);
+
+        log.info("useAsMapMethod: add map.putIfAbsent, then cache size is -> {}, result is -> {}",
+                cache.size(),
+                result);
+    }
+
+    // use callable to add entries to cache
+    private static void useCallable (){
+        createCache();
+        try {
+            log.info("useCallable: invoke cache.get(key, Callable<V>) first");
+            cache.get(key, () -> doThingsTheHardWay(key));
+            log.info("useCallable: invoke cache.get(key, Callable<V>) second");
+            cache.get(key, () -> doThingsTheHardWay(key)); // the second time will not perform callable.call()
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        log.info("useCallable: add cache.get(key, Callable<V>), then cache size is -> {}, result is -> {}",
+                cache.size(),
+                result);
+    }
+
+    private static Result<String> doThingsTheHardWay (String key){
+        try {
+            log.info("doThingsTheHardWay, key is -> {}", key);
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }≠
+        return result;
+    }
+
+}
